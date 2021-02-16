d22aebf68afe7c0a17d1d07dbcb62f7dc3ed8ea6
Cayo Puigdefabregas <cayo@ribaguifi.com>
2021-02-16 13:55:16 +0100

    install and up services

--- examples/init_project.sh
+++ examples/init_project.sh
@@ -9,15 +9,16 @@ cd panel
 python3 manage.py setupcronbeat
 python3 manage.py syncperiodictasks
 
-#sudo apt-get install -y rabbitmq-server
+sudo apt-get install -y rabbitmq-server
+sudo python3 manage.py setupcelery --username orchestra
 
 sudo python3 manage.py setuplog
 
 python3 manage.py collectstatic --noinput
-#sudo apt-get install -y nginx-full uwsgi uwsgi-plugin-python3
-#sudo python3 manage.py setupnginx --user orchestra
+sudo apt-get install -y nginx-full uwsgi uwsgi-plugin-python3
+sudo python3 manage.py setupnginx --user orchestra
 
-#sudo python3 manage.py startservices
+sudo python3 manage.py startservices
 
 
 # python3 panel/manage.py migrate
