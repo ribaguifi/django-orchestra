da29e8686096b158c5a7f8585d237542a1ece083
Santiago Lamora <santiago@ribaguifi.com>
2021-02-19 13:10:00 +0100

    Fix DEFAULT_FILTER_BACKENDS setting.

--- orchestra/conf/project_template/project_name/settings.py
+++ orchestra/conf/project_template/project_name/settings.py
@@ -228,7 +228,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': (
-        ('rest_framework.filters.DjangoFilterBackend',)
+        ('django_filters.rest_framework.DjangoFilterBackend',)
     ),
 }
 
