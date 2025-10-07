85901ffdb4a996e607000928b5bd602402753af0
Santiago Lamora <santiago@ribaguifi.com>
2025-10-07 13:41:56 +0200

    Handle existing remote contacts: update or create
    First approach: checking TAX NUMBER

--- orchestra/contrib/b2brouter/management/commands/sync_contacts.py
+++ orchestra/contrib/b2brouter/management/commands/sync_contacts.py
@@ -2,6 +2,7 @@ import swagger_client
 from django.core.management.base import BaseCommand
 from orchestra.contrib.b2brouter import settings
 from orchestra.contrib.bills.models import BillContact
+from orchestra.contrib.contacts.models import Contact
 from swagger_client.rest import ApiException
 
 
@@ -13,9 +14,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.init_api()
+        self.remote_contacts = self.fetch_remote_contacts()
+
         qs = self.retrieve_local_contacts()
-        for contact in qs[:1]:
-            self.create_remote_contact(contact)
+        for contact in qs:
+            contact.taxcode = f"{contact.country}{contact.vat}"
+            print(contact.taxcode)
+            update = contact.taxcode in self.remote_contacts
+            self.sync_remote_contact(contact, update=update)
 
     def retrieve_local_contacts(self):
         # TODO(@slamora): filter only active accounts???
@@ -25,17 +31,24 @@ class Command(BaseCommand):
     def init_api(self):
         # Configure API key authorization: api_key
         configuration = swagger_client.Configuration()
-        configuration.api_key['X-B2B-API-Key'] = settings.B2BROUTER_API_KEY
+        configuration.api_key["X-B2B-API-Key"] = settings.B2BROUTER_API_KEY
         configuration.host = settings.B2BROUTER_API_URL
 
         # create an instance of the API class
-        api_instance = swagger_client.ContactsApi(
-            swagger_client.ApiClient(configuration))
+        api_instance = swagger_client.ContactsApi(swagger_client.ApiClient(configuration))
 
         self.api = api_instance
 
-    def create_remote_contact(self, contact):
-        contact_info = contact.account.contacts.get(email_usages=["BILLING"])
+    def sync_remote_contact(self, contact, update=False):
+        try:
+            contact_info = contact.account.contacts.get(email_usages=["BILLING"])
+        except Contact.DoesNotExist:
+            print(f"  No billing contact found for account {contact.account}. Skipping.")
+            return None
+        except Contact.MultipleObjectsReturned:
+            print(f"  Multiple billing contacts found for account {contact.account}. Using the first one.")
+            contact_info = contact.account.contacts.filter(email_usages=["BILLING"]).first()
+
         billing_email = contact_info.email
         billing_phone = contact_info.phone or contact_info.phone2
 
@@ -56,10 +69,41 @@ class Command(BaseCommand):
             }
         }
 
-        try:
-            api_response = self.api.create_contact(
-                account=settings.B2BROUTER_ACCOUNT_ID, format="json", body=body)
-            return api_response
-        except ApiException as e:
-            print("Exception when calling ContactsApi->create_contact: %s\n" % e)
-            return None
+        if update:
+            contact_id = self.remote_contacts[contact.taxcode].id
+            try:
+                api_response = self.api.put_contact(id=contact_id, format="json", body=body)
+                return api_response
+            except ApiException as e:
+                print("Exception when calling ContactsApi->update_contact: %s\n" % e)
+                return None
+        else:
+            try:
+                api_response = self.api.create_contact(account=settings.B2BROUTER_ACCOUNT_ID, format="json", body=body)
+                return api_response
+            except ApiException as e:
+                print("Exception when calling ContactsApi->create_contact: %s\n" % e)
+                return None
+
+    def fetch_remote_contacts(self):
+        response = []
+        offset = 0
+        limit = 25
+        while True:
+            try:
+                api_response = self.api.get_contacts(
+                    account=settings.B2BROUTER_ACCOUNT_ID,
+                    format="json",
+                    limit=limit,
+                    offset=offset,
+                )
+                response.extend(api_response.clients)
+                total_count = api_response.total_count
+                offset += limit
+                if offset >= total_count:
+                    break
+            except ApiException as e:
+                print("Exception when calling ContactsApi->get_contacts: %s\n" % e)
+                raise
+
+        return {c.taxcode: c for c in response if c.taxcode}
