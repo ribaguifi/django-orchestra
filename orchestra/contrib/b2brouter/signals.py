bf0c10501c815c400dddb4bfeab5e91418698f9f
Santiago Lamora <santiago@ribaguifi.com>
2025-11-17 11:43:21 +0100

    Sync with b2brouter BillContact on post_save()

---
+++ orchestra/contrib/b2brouter/signals.py
@@ -0,0 +1,23 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from orchestra.contrib.b2brouter.management.commands.sync_contacts import Command as SyncCommand
+from orchestra.contrib.b2brouter.models import B2BContact
+
+
+@receiver(post_save, sender='bills.BillContact')
+def bill_contact_saved(sender, instance, **kwargs):
+    created = kwargs.get('created', False)
+    if created:
+        b2bcontact = B2BContact(orchestra_contact=instance)
+        update = False
+    else:
+        try:
+            b2bcontact = instance.b2bcontact
+            update = b2bcontact.remote_id is not None
+        except B2BContact.DoesNotExist:
+            b2bcontact = B2BContact(orchestra_contact=instance)
+            update = False
+
+    cmd = SyncCommand()
+    cmd.init_api()
+    cmd.sync_remote_contact(b2bcontact, update=update)
