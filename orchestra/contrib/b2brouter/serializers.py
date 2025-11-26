f43efc5770644dae00efdcbd2b6d1c11f7e9fff3
Santiago Lamora <santiago@ribaguifi.com>
2025-11-26 12:34:43 +0100

    Sync with B2BRouter on Invoice/Bill post_save

---
+++ orchestra/contrib/b2brouter/serializers.py
@@ -0,0 +1,69 @@
+from orchestra.contrib.b2brouter.models import B2BContact
+from orchestra.contrib.b2brouter import api
+
+# TODO serialize Bill to b2brouter invoice format
+class BillSerializer(object):
+    # TODO(@slamora): complete series & vat_percent mapping
+    SERIES_MAPPING = {
+        'FEE': 'S',
+    }
+    SERIES_MAPPING_DEFAULT = 'F'
+
+    SEPA_DIRECT_DEBIT_PAYMENT_METHOD = 59  # UBL: 59
+
+    def __init__(self, bill):
+        self.instance = bill
+
+    @property
+    def data(self):
+        if not hasattr(self, '_data'):
+            self._data = self.to_representation()
+        return self._data
+
+    def to_representation(self):
+        instance = self.instance
+
+        return {
+                "type": "IssuedInvoice",
+                "series_code": self.get_series(),
+                "number": instance.number,
+                "contact_id": self.get_contact_id(),
+                # TODO(@slamora): is this the desired date?
+                "date": instance.created_on.isoformat(),
+                "due_date": instance.get_due_date(),
+                "payment_method": self.SEPA_DIRECT_DEBIT_PAYMENT_METHOD,
+                "invoice_lines_attributes": self.get_lines_attributes(),
+        }
+
+    def get_contact_id(self):
+        # TODO: Implement contact ID retrieval logic
+        self.instance.buyer
+        try:
+            return self.instance.buyer.b2bcontact.remote_id
+        except B2BContact.DoesNotExist:
+            # TODO(@slamora): Handle missing B2BContact --> create or error?
+            remote_id = api.sync_remote_contact(self.instance.buyer, update=False)
+            return remote_id
+
+    def get_series(self):
+        return self.SERIES_MAPPING.get(self.instance.type, self.SERIES_MAPPING_DEFAULT)
+
+    def get_vat_percent(self):
+        if self.instance.type == 'FEE':
+            return 0
+        return 21
+
+    def get_lines_attributes(self):
+        lines = []
+        for index, item in enumerate(self.instance.lines.all()):
+            line = {
+                "position": index + 1,
+                "quantity": str(item.quantity),
+                "price": str(item.rate),
+                "description": item.description,
+                # "taxes_attributes": str(item.tax),
+                "invoicing_period_start": item.order_billed_on.isoformat() if item.order_billed_on else None,
+                "invoicing_period_end": item.order_billed_until.isoformat() if item.order_billed_until else None,
+            }
+            lines.append(line)
+        return lines
--- orchestra/contrib/b2brouter/signals.py
+++ orchestra/contrib/b2brouter/signals.py
@@ -1,12 +1,17 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from orchestra.contrib.b2brouter.management.commands.sync_contacts import Command as SyncCommand
-from orchestra.contrib.b2brouter.models import B2BContact
+from orchestra.contrib.b2brouter import settings
+from orchestra.contrib.b2brouter.api import get_invoice_api_instance
+from orchestra.contrib.b2brouter.management.commands.sync_contacts import (
+    Command as SyncCommand,
+)
+from orchestra.contrib.b2brouter.models import B2BContact, B2BInvoice
+from orchestra.contrib.b2brouter.serializers import BillSerializer
 
 
-@receiver(post_save, sender='bills.BillContact')
+@receiver(post_save, sender="bills.BillContact")
 def bill_contact_saved(sender, instance, **kwargs):
-    created = kwargs.get('created', False)
+    created = kwargs.get("created", False)
     if created:
         b2bcontact = B2BContact(orchestra_contact=instance)
         update = False
@@ -21,3 +26,51 @@ def bill_contact_saved(sender, instance, **kwargs):
     cmd = SyncCommand()
     cmd.init_api()
     cmd.sync_remote_contact(b2bcontact, update=update)
+
+
+# TODO(@slamora):
+# Bill type mapping
+# - Handle bill.number (to match remote system format if needed)
+# - Serialize line items
+# - Taxes handling
+# - Status changes??? Or it will be managed only remotely?
+
+
+@receiver(post_save, sender="bills.Bill")
+def bill_saved(sender, instance, **kwargs):
+    print(f"Bill saved: {instance}")
+    # Implement any necessary synchronization logic for Bill here
+    try:
+        remote = instance.b2binvoice
+        update = remote.remote_id is not None
+    except B2BInvoice.DoesNotExist:
+        remote = B2BInvoice(orchestra_bill=instance)
+        update = False
+
+    # TODO(@slamora): post_save Bill is triggered before lines are saved
+    sync_remote_invoice(instance, remote, update)
+
+
+def sync_remote_invoice(instance, remote_invoice, update):
+
+    print(f"Syncing remote invoice for Bill ID {instance.id}, update={update}")
+
+    invoice_data = BillSerializer(instance).data
+    payload = {
+        "send_after_import": False,
+        "ack": False,
+        "invoice": invoice_data,
+    }
+
+    api_instance = get_invoice_api_instance()
+    if update:
+        response = api_instance.put_invoice(
+            format="json", id=remote_invoice.remote_id, body=payload
+        )
+    else:
+        response = api_instance.create_invoice(
+            account=settings.B2BROUTER_ACCOUNT_ID, format="json", body=payload
+        )
+        remote_invoice.remote_id = response.id
+
+    remote_invoice.save()
