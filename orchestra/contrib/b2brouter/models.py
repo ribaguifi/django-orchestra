e8469d96c75f9ee3d144050dc23ec4fbdcaaaf29
Santiago Lamora <santiago@ribaguifi.com>
2025-12-18 10:26:50 +0100

    Refactor: DRY on models definition

--- orchestra/contrib/b2brouter/models.py
+++ orchestra/contrib/b2brouter/models.py
@@ -1,26 +1,31 @@
 from django.db import models
+
 from orchestra.contrib.bills.models import BillContact
 
 
-class B2BContact(models.Model):
+class SyncedModel(models.Model):
     class Status:
         SYNCED = "synced"
         ERROR = "error"
         PENDING = "pending"
         WARNING = "warning"
 
+    remote_id = models.BigIntegerField(null=True)
+    first_synced_at = models.DateTimeField(auto_now_add=True)
+    last_synced_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, default=Status.SYNCED)
+    message = models.TextField(blank=True, default="")
+
+    class Meta:
+        abstract = True
+
+
+class B2BContact(SyncedModel):
     orchestra_contact = models.OneToOneField(
         BillContact,
         on_delete=models.CASCADE,
         primary_key=True,
     )
-    remote_id = models.BigIntegerField(null=True)
-
-    first_synced_at = models.DateTimeField(auto_now_add=True)
-    last_synced_at = models.DateTimeField(auto_now=True)
-
-    status = models.CharField(max_length=20, default=Status.SYNCED)
-    message = models.TextField(blank=True, default="")
 
     def __str__(self):
         return f"Contact({self.orchestra_contact} - {self.remote_id})"
@@ -29,19 +34,12 @@ class B2BContact(models.Model):
         unique_together = (("orchestra_contact", "remote_id"),)
 
 
-class B2BInvoice(models.Model):
+class B2BInvoice(SyncedModel):
     orchestra_bill = models.OneToOneField(
-        'bills.Bill',
+        "bills.Bill",
         on_delete=models.CASCADE,
         primary_key=True,
     )
-    remote_id = models.BigIntegerField(null=True)
-
-    first_synced_at = models.DateTimeField(auto_now_add=True)
-    last_synced_at = models.DateTimeField(auto_now=True)
-
-    status = models.CharField(max_length=20, default=B2BContact.Status.SYNCED)
-    message = models.TextField(blank=True, default="")
 
     def __str__(self):
         return f"Invoice({self.orchestra_bill} - {self.remote_id})"
